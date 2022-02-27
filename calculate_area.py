1e587c169f73cf10790e62a438488301efd25025
KarolisKrakys <itskaroliukas@gmail.com>
2022-02-27 02:58:14 -0600

    asdasdasd

--- calculate_area.py
+++ calculate_area.py
@@ -1,4 +1,8 @@
 import geopy.distance
-
+from math import sqrt
 def calculate_area(c1, c2):
     return geopy.distance.distance(c1, c2).km
+
+def calculate_side(diag):
+    return sqrt(2)*(diag/2)
+
